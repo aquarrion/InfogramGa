1b7f2a55bcf36f97234d647e48362500d16e3554
aquarrion <garyaguilar300@gmail.com>
2021-10-03 03:23:12 -0400

    CircleImage&SimpleViewBehavior

--- app/src/main/java/com/aquarrion/infogram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/aquarrion/infogram/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.aquarrion.infogram.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aquarrion.infogram.R;
+import com.aquarrion.infogram.adapter.CardViewAdapter;
+import com.aquarrion.infogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,51 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //toolbar
+        showToolBar("", false, view);
+
+        //recyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_profile);
+
+        //layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(buidImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagene
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("https://th.bing.com/th/id/R.26bf1225572d805b6055d680528ce180?rik=6vL3Qxu0wKKiVA&riu=http%3a%2f%2ffotos.e-veracruz.mx%2fturismo_23.jpg&ehk=240UYp7RFdkOg9FozN46fJ%2fMzcMKPQQm6n4J%2fV4%2bWgU%3d&risl=&pid=ImgRaw&r=0","Carlos Valdivia","2 dias","1 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.58dc196848c318e0f30c06e555df9004?rik=KAYuaL0peNg%2fmw&pid=ImgRaw&r=0","Luis Perez","3 dias","6 me gusta"));
+        images.add(new Image("https://www.aseguratuviaje.com.mx/images/Lugares-turisticos-de-China.jpg","Fernando Torrico","5 dias","3 me gusta"));
+        images.add(new Image("https://negociosperu.pe/assets/sources/articulos/contenido/junio%202019/turismo%20y%20gastronomia/destacados07-06-19.jpg","Maria Torrez","7 dias","4 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.ed5bddcd18fbadfc823aa769e266aaf7?rik=%2f9vBqrmE%2fkd6zA&riu=http%3a%2f%2fwww.planetacurioso.com%2fwp-content%2fuploads%2f2014%2f10%2fdiaynoche-imagenes-720x420.jpg&ehk=edNhsPmTS5fXo99uQ0HoQectwS9rlbd7EIXsSmnqV08%3d&risl=&pid=ImgRaw&r=0","Rolando Morales","1 dias","5 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.839df82660d8d68046ea539a6e93ef35?rik=bz0voPtC%2bnc%2bDQ&pid=ImgRaw&r=0","William Jaimes","4 dias","12 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.ffa7d7ebb9a9521f5cf71e23479668a0?rik=Ziipz6fkHvfqPg&pid=ImgRaw&r=0","Roberto Paniagua","3 dias","13 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.647f59704b38f5cb87d9b37dd99b70b5?rik=ABrckDzjKEZ45Q&pid=ImgRaw&r=0","Lorena Herrera","6 dias","15 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.ef2bc6c95fc7211e56bdf375a7aa8b70?rik=%2fdYUBKzUZ2J%2f3A&pid=ImgRaw&r=0","Gary Aguilar","5 dias","33 me gusta"));
+        images.add(new Image("https://th.bing.com/th/id/R.9b82e5229c844d022bf865f03c16c419?rik=Up3qebNuyxwx2Q&riu=http%3a%2f%2frh-destinations.com%2fes%2fwp-content%2fuploads%2fsites%2f6%2f2014%2f06%2fParisFrancia_RoyalHoliday.jpg&ehk=%2fF0GfC%2fblis5PdloqBEqXOBwHCcREaytDD7fIB0olQ4%3d&risl=&pid=ImgRaw&r=0","Sara Ralde","4 dias","50 me gusta"));
+
+
+        return images;
     }
 }
